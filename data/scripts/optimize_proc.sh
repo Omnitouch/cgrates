47a0e81be9acfea2f3eac710e8047a8a28168ef3
davidkneipp <david@davidkneipp.com>
2023-01-24 11:56:03 +1000

    Clear repo for rollback to 10.2

--- data/scripts/optimize_proc.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env sh
-
-for i in /sys/devices/system/cpu/cpu[0-9]
-do
-    echo performance > $i/cpufreq/scaling_governor
-done
-
--- data/vagrant/golang.sh
+++
@@ -1,3 +0,0 @@
-export GOROOT=/home/vagrant/go GOPATH=/home/vagrant/code
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
